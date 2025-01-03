c5de92e2a2a7fca787864f7cba699ec4735917fc
Julian Andres Klode <jak@debian.org>
2025-01-03 20:51:16 +0100

    install-progress: Smaller discrete progress bar

--- test/libapt/install_progress_test.cc
+++ test/libapt/install_progress_test.cc
@@ -13,33 +13,31 @@ TEST(InstallProgressTest, FancyGetTextProgressStr)
 
    char *originalLocale = setlocale(LC_ALL, "C");
    EXPECT_EQ(60u, p.GetTextProgressStr(0.5, 60).size());
-   EXPECT_EQ("[#.]", p.GetTextProgressStr(0.5, 4));
-   EXPECT_EQ("[..........]", p.GetTextProgressStr(0.0, 12));
-   EXPECT_EQ("[#.........]", p.GetTextProgressStr(0.1, 12));
-   EXPECT_EQ("[####......]", p.GetTextProgressStr(0.4999, 12));
-   EXPECT_EQ("[#####.....]", p.GetTextProgressStr(0.5001, 12));
-   EXPECT_EQ("[#########.]", p.GetTextProgressStr(0.9001, 12));
-   EXPECT_EQ("[##########]", p.GetTextProgressStr(1.0, 12));
+   EXPECT_EQ("##--", p.GetTextProgressStr(0.5, 4));
+   EXPECT_EQ("------------", p.GetTextProgressStr(0.0, 12));
+   EXPECT_EQ("#-----------", p.GetTextProgressStr(0.1, 12));
+   EXPECT_EQ("#####-------", p.GetTextProgressStr(0.4999, 12));
+   EXPECT_EQ("######------", p.GetTextProgressStr(0.5001, 12));
+   EXPECT_EQ("##########--", p.GetTextProgressStr(0.9001, 12));
+   EXPECT_EQ("############", p.GetTextProgressStr(1.0, 12));
 
    // deal with incorrect inputs gracefully (or should we die instead?)
-   EXPECT_EQ("[..........]", p.GetTextProgressStr(-1.0, 12));
-   EXPECT_EQ("[##########]", p.GetTextProgressStr(2.0, 12));
+   EXPECT_EQ("------------", p.GetTextProgressStr(-1.0, 12));
+   EXPECT_EQ("############", p.GetTextProgressStr(2.0, 12));
 
    setlocale(LC_ALL, "C.UTF-8");
 
-   EXPECT_EQ("[█ ]", p.GetTextProgressStr(0.5, 4));
-   EXPECT_EQ("[          ]", p.GetTextProgressStr(0.0, 12));
-   EXPECT_EQ("[█         ]", p.GetTextProgressStr(0.1, 12));
-   EXPECT_EQ("[████      ]", p.GetTextProgressStr(0.4, 12));
-   EXPECT_EQ("[████▉     ]", p.GetTextProgressStr(0.4999, 12));
-   EXPECT_EQ("[█████     ]", p.GetTextProgressStr(0.5000, 12));
-   EXPECT_EQ("[█████     ]", p.GetTextProgressStr(0.5001, 12));
-   EXPECT_EQ("[█████████ ]", p.GetTextProgressStr(0.9001, 12));
-   EXPECT_EQ("[██████████]", p.GetTextProgressStr(1.0, 12));
+   EXPECT_EQ("∎∎——", p.GetTextProgressStr(0.5, 4));
+   EXPECT_EQ("————————————", p.GetTextProgressStr(0.0, 12));
+   EXPECT_EQ("∎———————————", p.GetTextProgressStr(0.1, 12));
+   EXPECT_EQ("∎∎∎∎∎———————", p.GetTextProgressStr(0.4999, 12));
+   EXPECT_EQ("∎∎∎∎∎∎——————", p.GetTextProgressStr(0.5001, 12));
+   EXPECT_EQ("∎∎∎∎∎∎∎∎∎∎——", p.GetTextProgressStr(0.9001, 12));
+   EXPECT_EQ("∎∎∎∎∎∎∎∎∎∎∎∎", p.GetTextProgressStr(1.0, 12));
 
    // deal with incorrect inputs gracefully (or should we die instead?)
-   EXPECT_EQ("[          ]", p.GetTextProgressStr(-1.0, 12));
-   EXPECT_EQ("[██████████]", p.GetTextProgressStr(2.0, 12));
+   EXPECT_EQ("————————————", p.GetTextProgressStr(-1.0, 12));
+   EXPECT_EQ("∎∎∎∎∎∎∎∎∎∎∎∎", p.GetTextProgressStr(2.0, 12));
 
    setlocale(LC_ALL, originalLocale);
 }
