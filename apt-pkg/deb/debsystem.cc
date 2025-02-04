live-clones/apt

commit 8e6a86aa3bc6d5a9013bb42fccea9bbce3fd97e2

apt-pkg/deb/debsystem.cc: Fall back to "dpkg" when Dir::Bin::dpkg is not found
The custom dpkg binary may be removed by apt and that breaks the end of
the operation. This can be observed when using eatmydata as dpkg and
removing the symlink to eatmydata when removing the apt-eatmydata package.

apt-pkg/deb/debsystem.cc
@@ -392,7 +392,16 @@ std::string debSystem::StripDpkgChrootDirectory(std::string const &File)/*{{{*/
 									/*}}}*/
 std::string debSystem::GetDpkgExecutable()				/*{{{*/
 {
-   return StripDpkgChrootDirectory(_config->Find("Dir::Bin::dpkg","dpkg"));
+   std::string dpkg_executable = _config->Find("Dir::Bin::dpkg","dpkg");
+   if (APT::String::Startswith(dpkg_executable, "/")) {
+      struct stat buf;
+      if (stat(dpkg_executable.c_str(), &buf) != 0) {
+         _error->Warning(_("The dpkg executable set in Dir::Bin::dpkg is "
+                          "missing, falling back to using default dpkg."));
+        dpkg_executable = "dpkg";
+      }
+   }
+   return StripDpkgChrootDirectory(dpkg_executable);
 }
 									/*}}}*/
 std::vector<std::string> debSystem::GetDpkgBaseCommand()		/*{{{*/

apt-pkg/deb/dpkgpm.cc
@@ -1773,7 +1773,6 @@ bool pkgDPkgPM::Go(APT::Progress::PackageManager *progress)
 
    // this loop is runs once per dpkg operation
    vector<Item>::const_iterator I = List.cbegin();
-   BuildDpkgCall Args;
    while (I != List.end())
    {
       // Do all actions with the same Op in one run
@@ -1795,7 +1794,7 @@ bool pkgDPkgPM::Go(APT::Progress::PackageManager *progress)
       else
 	 J = std::find_if(J, List.cend(), [&J](Item const &I) { return I.Op != J->Op; });
 
-      Args.clearCallArguments();
+      BuildDpkgCall Args;
       Args.reserve((J - I) + 10);
 
       int fd[2];

commit 9fcda4b6dd2ffd898ca29dcb2507a0f0e3f21fbd

dpkg: Pass --admindir if a different one is specified
Otherwise if we set e.g. Dir, we end up with the foreign
architectures from the host which is wrong.

Fixes: 8e6a86aa3bc6d5a9013bb42fccea9bbce3fd97e2

apt-pkg/deb/debsystem.cc
@@ -420,6 +420,13 @@ std::vector<std::string> debSystem::GetDpkgBaseCommand()		/*{{{*/
 	 Args.push_back(Opts->Value);
       }
    }
+   auto status = _config->FindFile("Dir::State::status");
+   auto admindir = StripDpkgChrootDirectory(flNotFile(status));
+   if (admindir != "/var/lib/dpkg/")
+   {
+      Args.push_back("--admindir");
+      Args.push_back(admindir);
+   }
    return Args;
 }
 									/*}}}*/